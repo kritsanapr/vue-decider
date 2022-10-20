8576bd22213437f1dedbfc44f9a18f10eeb913d5
kritsanapr <kritsana.pr.41@gmail.com>
2022-10-20 21:51:07 +0700

    Commit project

---
+++ README.md
@@ -0,0 +1,31 @@
+# decider
+
+Simple Vue decider mini project for learn about transition and animation.
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container">
+    <transition name="fade" appear mode="out-in">
+      <component
+        :is="screens[position]"
+        :question="question"
+        :result="result"
+        @goto="handleGoTo"
+        @question="handleQuestion"
+        @showResult="showResult"
+        @startOver="startOver"
+        @handleToast="handleToast"
+      />
+    </transition>
+  </div>
+</template>
+
+<script>
+import appInitial from "./components/initial.vue";
+import appConfirm from "./components/confirm.vue";
+import appResults from "./components/results.vue";
+
+export default {
+  components: {
+    appInitial,
+    appConfirm,
+    appResults,
+  },
+  data() {
+    return {
+      list: [
+        "Yes",
+        "No",
+        "Maybe",
+        "Not sure..try again",
+        "Ask a friend",
+        "Call the police",
+      ],
+      screens: ["appInitial", "appConfirm", "appResults"],
+      position: 0,
+      question: "",
+      result: "",
+    };
+  },
+  methods: {
+    handleToast(values) {
+      this.$toast.show(values.message, {
+        type: values.type,
+        position: "top",
+        duration: 2000,
+        pauseOnHover: false,
+      });
+    },
+    handleGoTo(position) {
+      this.position = position;
+    },
+    handleQuestion(question) {
+      this.question = question;
+    },
+    getRandomValue() {
+      return this.list[Math.floor(Math.random() * this.list.length)];
+    },
+    generateResult() {
+      let rand = this.getRandomValue();
+
+      if (this.result !== "") {
+        while (rand === this.result) {
+          rand = this.getRandomValue();
+        }
+      }
+
+      this.result = rand;
+    },
+    showResult() {
+      this.generateResult();
+    },
+    startOver() {
+      this.position = 0;
+      this.question = "";
+      this.result = "";
+    },
+  },
+};
+</script>
+
+<style>
+@import "./assets/style.css";
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: 0.5s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: 0.5s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
